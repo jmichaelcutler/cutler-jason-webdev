5a1076d74f7f8853865012ee8ee8eb756fe73701
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-18 03:17:07 -0700

    Completed login.controller.client.js

--- public/assignment/views/user/controllers/login.conroller.client.js
+++ public/assignment/views/user/controllers/login.conroller.client.js
@@ -1,3 +1,19 @@
-/**
- * Created by jmich on 6/18/2017.
- */
+(function () {
+    angular
+        .module('WebAppMaker')
+        .controller('loginController', loginController);
+
+    function loginController($location, userService) {
+        var model = this;
+
+        model.login = function (username, password) {
+            var found = userService.findUserByCredentials(username, password);
+
+            if (found !== null) {
+                $location.url('/user/' + found._id)
+            } else {
+                model.message = "Username " + username + " not found."
+            }
+        };
+    }
+})();

b805bf272233248b4d8f255000b3ac168f214ae6
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-19 04:09:31 -0700

    Binding controllers

---
+++ public/assignment/views/widget/controllers/widget.controller.client.js
@@ -0,0 +1,23 @@
+(function () {
+    angular
+        .module('WebAppMaker')
+        .controller('ListWidgetController', ListWidgetController);
+    angular
+        .module('WebAppMaker')
+        .controller('NewWidgetController', NewWidgetController);
+    angular
+        .module('WebAppMaker')
+        .controller('EditWidgetController', EditWidgetController);
+
+    function ListWidgetController() {
+        var vm = this;
+    }
+
+    function NewWidgetController() {
+        var vm = this;
+    }
+
+    function EditWidgetController() {
+        var vm = this;
+    }
+})();

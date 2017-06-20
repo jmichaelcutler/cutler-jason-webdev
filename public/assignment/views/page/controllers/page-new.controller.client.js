d472ff13bd81abd68e7be0675df171aa10122b90
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-20 05:15:59 -0700

    Updaged page-edit for event handling

--- public/assignment/views/page/controllers/page-new.controller.client.js
+++ public/assignment/views/page/controllers/page-new.controller.client.js
@@ -3,7 +3,20 @@
         .module('WebAppMaker')
         .controller('NewPageController', NewPageController);
 
-    function NewPageController() {
+    function NewPageController($routeParams, $location, PageService) {
         var vm = this;
+        vm.userId = $routeParams["userId"];
+        vm.websiteId = $routeParams["websiteId"];
+        vm.createPage = createPage;
+
+        function init() {
+            vm.pages = PageService.findPagesByWebsiteId(vm.websiteId);
+        }
+
+        init();
+
+        function createPage() {
+            PageService.createPage(vm.websiteId, vm.page);
+        }
     }
 })();

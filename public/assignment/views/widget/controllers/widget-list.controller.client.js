0f759966a5956430d81f3809369a12e61867229b
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-20 06:10:06 -0700

    Updated widget-list controller for event handling.

--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -2,14 +2,39 @@
     angular
         .module('WebAppMaker')
         .controller('WidgetListController', WidgetListController);
-
-    function WidgetListController($routeParams, WidgetService) {
+    function WidgetListController($routeParams, $sce, WidgetService) {
         var vm = this;
-        vm.pageId = $routeParams["pageId"];
+        vm.userID = $routeParams.userId;
+        vm.websiteId = $routeParams.websiteId;
+        vm.pageId = $routeParams.pageId;
+        vm.trustHtml = trustHtml;
+        vm.embedYouTube = embedYouTube;
+        vm.trustImage = trustImage;
+        vm.getWidgetUrlForType = getWidgetUrlForType;
+
         function init() {
             vm.widgets = WidgetService.findWidgetsByPageId(vm.pageId);
         }
-
         init();
+
+        function trustHtml(text) {
+            return $sce.trustAsHtml(text);
+        }
+
+        function trustImage(url) {
+            return $sce.trustAsUrl(url);
+        }
+
+        function embedYouTube(url) {
+            var embedUrl = 'https://www.youtube.com/embed/';
+            var youTubeLinkParts = url.split('/');
+            var id = youTubeLinkParts[youTubeLinkParts.length - 1];
+            embedUrl += id;
+            return $sce.trustAsResourceUrl(embedUrl);
+        }
+
+        function getWidgetUrlForType(type) {
+            return 'views/widget/templates/widget-' + type.toLowerCase() + '.view.client.html';
+        }
     }
 })();

1392f4d8d88a3e84d95f7b4aaa95774cae4d97e7
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-20 03:58:17 -0700

    Page edit controller event handler

--- public/assignment/views/page/controllers/page-edit.controller.client.js
+++ public/assignment/views/page/controllers/page-edit.controller.client.js
@@ -5,11 +5,25 @@
 
     function EditPageController($routeParams, PageService) {
         var vm = this;
+        vm.userId = $routeParams.userId;
+        vm.websiteId = $routeParams.userId;
         vm.pageId = $routeParams["pageId"];
+        vm.updatePage = updatePage;
+        vm.deletePage = deletePage;
         function init() {
             vm.page = PageService.findPageById(vm.pageId);
         }
 
         init();
+
+        function updatePage(page) {
+            PageService.updatePage(vm.pageId, page);
+            $location.url('/user/' + vm.userId + '/website/' + vm.websiteId + '/page');
+        }
+
+        function deletePage() {
+            PageService.deletePage(vm.pageId);
+            $location.url('/user/' + vm.userId + '/website/' + vm.websiteId + '/page');
+        }
     }
 })();

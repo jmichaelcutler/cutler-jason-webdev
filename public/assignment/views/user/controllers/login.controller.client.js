49411aa5642df21f1ce58cb4987fb7e3b81c4837
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-21 09:22:25 -0700

    Fixed login in login controller

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -4,16 +4,16 @@
         .controller("LoginController", LoginController);
 
     function LoginController($location, UserService) {
-        var model = this;
-        model.login = login;
+        var vm = this;
+        vm.login = login;
 
-        function login() {
-            var user = UserService.findUserByCredentials(model.username, model.password);
+        function login(username, password) {
+            var user = UserService.findUserByCredentials(username, password);
 
             if (user) {
                 $location.url("/user/" + user._id);
             } else {
-                model.message = "Unable to login";
+                vm.message = "Unable to login";
             }
         }
     }

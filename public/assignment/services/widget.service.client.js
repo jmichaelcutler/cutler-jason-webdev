1cd721612a0206323a6068a617ecfbee86f51585
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-30 11:26:26 -0700

    Updated widget.service.client for promises.

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -3,23 +3,8 @@
         .module("WebAppMaker")
         .factory("WidgetService", WidgetService);
 
-    function WidgetService() {
-        var widgets = [
-            {"_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
-            {"_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            {
-                "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-                "url": "http://lorempixel.com/400/200/"
-            },
-            {"_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
-            {"_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            {
-                "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E"
-            },
-            {"_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+    function WidgetService($http) {
 
-        ];
 
         return {
             createWidget: createWidget,
@@ -30,40 +15,43 @@
         };
 
         function createWidget(pageId, widget) {
-            widget.pageId = pageId;
-            widget._id = (new Date().getTime() + "");
-            widgets.push(widget);
+            var url = "/api/page/" + pageId + "widget";
+            return $http.post(url, widget)
+                .then(function (response) {
+                    return response.data;
+                });
         }
 
         function findWidgetsByPageId(pageId) {
-            var widgetList = [];
-            for (var w in widgets) {
-                if (widgets[w].pageId === pageId) {
-                    widgetList.push(widgets[w]);
-                }
-            }
-            return widgetList;
+            var url = "/api/page/" + pageId + "widget";
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
+                });
         }
 
         function findWidgetById(widgetId) {
-            for (var w in widgets) {
-                if (widgets[w]._id === widgetId) {
-                    return widgets[widgetId];
-                }
-            }
-            return null;
+            var url = "/api/widget/" + widgetId;
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
+                });
         }
 
         function updateWidget(widgetId, widget) {
-            widget._id = widgetId;
-            deleteWidget(widgetId);
-            widgets.push(widget);
+            var url = "/api/widget/" + widgetId;
+            return $http.put(url, widget)
+                .then(function (response) {
+                    return response.data;
+                });
         }
 
         function deleteWidget(widgetId) {
-            var victimWidget = findWidgetById(widgetId);
-            var index = widgets.indexOf(victimWidget);
-            widgets.splice(index, 1);
+            var url = "/api/widget/" + widgetId;
+            return $http.delete(url)
+                .then(function (response) {
+                    return response.data;
+                });
         }
     }
 })();

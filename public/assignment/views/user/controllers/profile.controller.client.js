8a6b4a1138991e29630e4f153127274711be675b
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-30 07:47:59 -0700

    Updated user profile controller

--- public/assignment/views/user/controllers/profile.controller.client.js
+++ public/assignment/views/user/controllers/profile.controller.client.js
@@ -6,22 +6,34 @@
         .module("WebAppMaker")
         .controller('ProfileController', ProfileController);
 
-    function ProfileController($routeParams, UserService) {
+    function ProfileController($location, $routeParams, UserService) {
         var vm = this;
         vm.updateUser = updateUser;
-        vm.userId = $routeParams["userId"];
+        vm.deleteUser = deleteUser;
+        var userId = $routeParams["userId"];
 
-        function init() {
-            vm.user = UserService.findUserById(vm.userId);
+        UserService
+            .findUserById(userId)
+            .then(renderUser);
+
+        function renderUser(response) {
+            vm.user = response.data;
         }
 
-        init();
+        function deleteUser(user) {
+            UserService
+                .deleteUser(user._id)
+                .then(function () {
+                    $location.url('/login');
+                });
+        }
 
-        function updateUser(newUser) {
-            var entry = UserService.updateUser(vm.userId, newUser);
-            if (!entry) {
-                vm.alert = "Error, user not updated."
-            }
+        function updateUser(user) {
+            UserService
+                .updateUser(user._id, user)
+                .then(function () {
+                    vm.message = "User updated successfully";
+                })
         }
     }
 

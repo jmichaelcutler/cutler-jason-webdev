92bfe7ce9eb400e80cd83f2b6f22dfc0e3bacd4f
jmichaelcutler <jmichaelcutler@gmail.com>
2017-06-30 16:15:29 -0700

    Updated website-edit.controller for promises

--- public/assignment/views/website/controllers/website-edit.controller.client.js
+++ public/assignment/views/website/controllers/website-edit.controller.client.js
@@ -10,17 +10,47 @@
         vm.deleteWebsite = deleteWebsite;
 
         function init() {
-            vm.website = WebsiteService.findWebsiteById(vm.websiteId);
+            WebsiteService
+                .findWebsiteById(vm.websiteId)
+                .then(function () {
+                    vm.websites = websites
+                }, findWebsiteError);
+
+            function findWebsiteError(error) {
+                vm.message = "An error occurred, could not find website"
+            }
+
+            // vm.website = WebsiteService.findWebsiteById(vm.websiteId);
         }
 
         init();
 
         function updateWebsite(website) {
-            WebsiteService.updateWebsite(vm.websiteId, website);
+            WebsiteService
+                .updateWebsite(vm.websiteId, website)
+                .then(function () {
+                    $location.url = "/user/" + vm.userId + "/website"
+                }, updateWebsiteError);
+
+            function updateWebsiteError(error) {
+                vm.message = "An error occurred, could not update website.";
+            }
+
+            // WebsiteService.updateWebsite(vm.websiteId, website);
         }
 
         function deleteWebsite() {
-            WebsiteService.deleteWebsite(vm.websiteId);
+            WebsiteService
+                .deleteWebsite(vm.websiteId)
+                .then(function () {
+                    location.url = "/user/" + vm.userId + "/website"
+                }, deleteError);
+
+            function deleteError(error) {
+                vm.message = "An error occurred, could not delete website.";
+            }
+
+            // WebsiteService.deleteWebsite(vm.websiteId);
         }
     }
 })();

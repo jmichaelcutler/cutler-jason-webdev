b628ad09faff1e79cbf43e21478d8d5cb08f5364
jmichaelcutler <jmichaelcutler@gmail.com>
2017-07-02 11:04:08 -0700

    Implemented image upload

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -1,10 +1,14 @@
 var app = require("../../express");
 
+var multer = require('multer'); // npm install multer --save
+var upload = multer({dest: __dirname + '/../../public/uploads'});
+
 app.post("/api/assignment/page/:pageId/widget", createWidget);
 app.get("/api/assignment/page/:pageId/widget", findAllWidgetsForPage);
 app.get("/api/assignment/widget/:widgetId", findWidgetById);
 app.put("/api/assignment/widget/:widgetId", updateWidget);
 app.delete("/api/assignment/widget/:widgetId", deleteWidget);
+app.post("/api/assignment/upload", upload.single('myFile'), uploadImage);
 
 function createWidget(req, res) {
     var pageId = req.params["pageId"];
@@ -58,4 +62,28 @@ function deleteWidget(req, res) {
         }
     }
     res.sendStatus(200);
+}
+
+function uploadImage(req, res) {
+    var widgetId = req.body.widgetId;
+    var width = req.body.width;
+    var myFile = req.file;
+
+    var userId = req.body.userId;
+    var websiteId = req.body.websiteId;
+    var pageId = req.body.pageId;
+
+    var originalname = myFile.originalname; // file name on user's computer
+    var filename = myFile.filename;     // new file name in upload folder
+    var path = myFile.path;         // full path of uploaded file
+    var destination = myFile.destination;  // folder where file is saved to
+    var size = myFile.size;
+    var mimetype = myFile.mimetype;
+
+    var widget = findWidgetById(widgetId);
+    widget.url = '/uploads/' + filename;
+
+    var callbackUrl = "/assignment/user/" + userId + "/website/" + websiteId + "/page/" + pageId + "/widget/" + widgetId;
+
+    res.redirect(callbackUrl);
 }

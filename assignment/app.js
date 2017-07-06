1c0147a0707d5279b86ac20e62dca323c1993a24
jmichaelcutler <jmichaelcutler@gmail.com>
2017-07-06 09:41:58 -0700

    Updated app.js

--- assignment/app.js
+++ assignment/app.js
@@ -3,7 +3,6 @@ module.exports = function (app) {
     require("./services/website.service.server");
     require("./services/page.service.server");
     require("./services/widget.service.server");
-    require("./directives/wbdv-sortable.js");
 };
 
 

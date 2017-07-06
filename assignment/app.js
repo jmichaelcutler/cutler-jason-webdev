6fa4a88aea1d309be1495d9c9eae63ced0e652c3
jmichaelcutler <jmichaelcutler@gmail.com>
2017-07-06 09:18:56 -0700

    Added flickr.service.client.js

--- assignment/app.js
+++ assignment/app.js
@@ -1,8 +1,8 @@
 module.exports = function (app) {
-    require("./services/user.service.server")(app);
-    require("./services/website.service.server")(app);
-    require("./services/page.service.server")(app);
-    require("./services/widget.service.server")(app);
+    require("./services/user.service.server");
+    require("./services/website.service.server");
+    require("./services/page.service.server");
+    require("./services/widget.service.server");
 };
 
 

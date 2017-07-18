1fc84eea709fbe31808a09805cfc9e850512db5a
jmichaelcutler <jmichaelcutler@gmail.com>
2017-07-17 20:36:35 -0700

    Included model requirements to models server

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -13,4 +13,11 @@ module.exports = function () {
     }
 
     mongoose.connect(connectionString);
+
+    return {
+        userModel: require("./user/user.model.server")(),
+        websiteModel: require("./website/website.model.server")(),
+        pageModel: require("./page/page.model.server")(),
+        widgetModel: require("./widget/widget.model.server")()
+    };
 };

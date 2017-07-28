eb001d5789ae5215ea7f261b32244b79a386c13e
jmichaelcutler <jmichaelcutler@gmail.com>
2017-07-27 19:33:28 -0700

    Added passport to server.js

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
+var passport = require('passport');
 
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./assignment/app.js")(app);
 

b1548308b8c88057e755bfff13b340979fee85b8
jmichaelcutler <jmichaelcutler@gmail.com>
2017-07-28 21:16:41 -0700

    Configure server, package.json for passport

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -5,22 +5,15 @@
 
     function LoginController($location, UserService) {
         var model = this;
-        model.login = function () {
+        model.login = login;
+
+        function login(user) {
             UserService
-                .findUserByCredentials(model.username, model.password)
-                .then(login, handleError);
-
-            function handleError(error) {
-                model.message = "Username " + username + " not found, please try again";
-            }
-
-            function login(found) {
-                if (found !== null) {
-                    $location.url("/user/" + found._id);
-                } else {
-                    model.message = "Username not found, please try again."
-                }
-            }
-        };
+                .login(user)
+                .then(function (response) {
+                    var user = response.data;
+                    $location.url = ("/profile");
+                });
+        }
     }
 })();

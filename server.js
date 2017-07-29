a29193970d7295c3da3d76c51f78d80b02b1ccac
jmichaelcutler <jmichaelcutler@gmail.com>
2017-07-28 21:06:13 -0700

    Configure server, package.json for passport

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-
+var passport = require('passport');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
-var passport = require('passport');
 
 
+app.use(cookieParser());
+app.use({secret: process.env.SESSION_SECRET});
 app.use(passport.initialize());
 app.use(passport.session());
 
 require("./assignment/app.js")(app);
+
 
 var port = process.env.PORT || 3000;
 

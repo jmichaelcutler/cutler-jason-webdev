5767e7bf78ff4a234dab2b9aecd48a35044b2b82
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-01 14:16:51 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var passport = require('passport');
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use({secret: process.env.SESSION_SECRET});
-
-// configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+var session = require('express-session');
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+
+var passport = require('passport');
 
 app.use(passport.initialize());
 app.use(passport.session());

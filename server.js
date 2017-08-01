7217c2eddeb4f33d492c592cfb6e396d66e107c1
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-01 14:08:18 -0700

    added cookie parser to package.json

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use({secret: process.env.SESSION_SECRET});
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
 
-app.use(cookieParser());
-app.use({secret: process.env.SESSION_SECRET});
 app.use(passport.initialize());
 app.use(passport.session());
 

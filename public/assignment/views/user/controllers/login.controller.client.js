39981b60e2496fadd2b4bc5225c63a4db5d0c785
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-02 17:37:25 -0700

    Moved logout to login controller.

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -6,6 +6,7 @@
     function LoginController($location, UserService) {
         var model = this;
         model.login = login;
+        model.logout = logout;
 
         function login(user) {
             if (user.username === null || user.password === null) {
@@ -16,6 +17,14 @@
                 .then(function (response) {
                     var user = response.data;
                     $location.url = ("/user/" + user._id);
+                }, handleError);
+        }
+
+        function logout(user) {
+            UserService
+                .logout(user)
+                .then(function (response) {
+                    $location.url("/");
                 }, handleError);
         }
 

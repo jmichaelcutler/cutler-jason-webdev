0be89687ad3f6239538caa019f675478ee708ccd
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-04 17:14:34 -0700

    Created project config skeleton

--- public/project/config.js
+++ public/project/config.js
@@ -1,3 +1,18 @@
-/**
- * Created by Jason on 8/3/2017.
- */
+(function () {
+    angular
+        .module("MusicDBApp")
+        .config(Config);
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "",
+                controller: "",
+                controllerAs: "project"
+            })
+            .when("/", {})
+            .when("default", {})
+            .when("/register", {})
+            .when("/profile", {})
+    }
+})();

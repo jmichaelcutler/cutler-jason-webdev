6839432dfec168ed2c181b748671f23d9a34ca30
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-10 21:24:16 -0700

    Added poc to server.js

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./assignment/app.js")(app);
-require("./public/poc/app.js")(app);
 
 
 var port = process.env.PORT || 3000;

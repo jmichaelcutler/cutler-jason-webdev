5906bf3dee61007438f40a5d71722ecb5953e19b
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-10 22:00:41 -0700

    Added poc to server.js

--- assignment/directives/wbdv-sortable.js
+++ assignment/directives/wbdv-sortable.js
@@ -1,6 +1,6 @@
 (function () {
     angular
-        .module("WebAppMaker")
+        .module("wbdv-sortable", [])
         .directive("wbdvDirectives", sortWidget);
 
     function sortWidget($http) {

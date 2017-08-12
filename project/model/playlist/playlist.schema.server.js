5a6887734a80745810dabdd1f25fee25d7aafa61
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-11 20:16:38 -0700

    Added name category to playlist schema

--- project/model/playlist/playlist.schema.server.js
+++ project/model/playlist/playlist.schema.server.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 var playlistSchema = mongoose.Schema({
+    name: String,
     _user: {type: mongoose.Schema.ObjectId, ref: "UserModel"},
     _songs: [{type: mongoose.Schema.ObjectId, ref: "SongModel"}],
     dateCreated: {type: Date, default: Date.now}

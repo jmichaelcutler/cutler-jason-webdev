264e85cdc68a81d71a98972f3464f8b3d7969173
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-12 18:08:47 -0700

    Added main page to config.js

--- public/project/services/search.service.client.js
+++ public/project/services/search.service.client.js
@@ -5,7 +5,7 @@
 
     function SearchService() {
 
-        var key = process.env.DISCOGS_KEY;
+        var key = process.env[DISCOGS_KEY];
         var secret = process.env.DISCOGS_SECRET;
         var baseURL = "https://api.discogs.com";
 

076ab77998265f6b63804cb772423a6f01141d6b
jmichaelcutler <jmichaelcutler@gmail.com>
2017-08-13 23:15:53 -0700

    Updated schemas for user

--- project/model/playlist/playlist.schema.server.js
+++ project/model/playlist/playlist.schema.server.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 var playlistSchema = mongoose.Schema({
     name: String,
-    _user: {type: mongoose.Schema.ObjectId, ref: "UserModel"},
-    _songs: [{type: mongoose.Schema.ObjectId, ref: "SongModel"}],
+    _user: {type: mongoose.Schema.Types.ObjectId, ref: "UserModel"},
+    _songs: [{type: mongoose.Schema.Types.ObjectId, ref: "SongModel"}],
     dateCreated: {type: Date, default: Date.now}
 }, {collection: "playlist"});
 
--- project/model/user/user.schema.server.js
+++ project/model/user/user.schema.server.js
@@ -1,16 +1,15 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-    //TODO add google properties
     username: {type: String, require: true, unique: true},
     password: {type: String, require: true},
     firstName: String,
     lastName: String,
     email: String,
     roles: {type: String, default: 'USER', enum: ['USER', 'ARTIST', 'ADMIN']},
-    dateCreated: {type: Date, default: Date.now}
-    // TODO Add playlists
-    // TODO Add reviews
+    dateCreated: {type: Date, default: Date.now},
+    _playlists: [{type: mongoose.Schema.Types.ObjectId, ref: "PlaylistModel"}],
+    _reviews: [{type: mongoose.Schema.Types.ObjectId, ref: "ReviewModel"}]
 }, {collection: "user"});
 
 module.exports = userSchema;
